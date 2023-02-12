d2e33f621210a4f9949475886ac7ed690c7dcca9
paulcervov <paulcervov@gmail.com>
2023-02-12 12:42:36 +0700

    Another loop task solved

--- for/task1.go
+++ for/task1.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// It is required to write a program, during which two natural numbers
+// A and B are read from the keyboard (each is not more than 100, A < B).
+// Print the sum of all numbers from A to B inclusive.
 func main() {
 	var a, b, i, sum int
 
--- for/task2.go
+++ for/task2.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// Write a program that finds the sum of two-digit numbers divisible by 8 in a sequence of numbers.
+// The program in the first line receives the number n - the number of numbers in the sequence,
+// in the second line - n numbers included in this sequence.
 func main() {
 	var i, digitCount, digit, digitSum int
 	fmt.Scan(&digitCount)
