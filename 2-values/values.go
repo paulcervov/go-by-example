0477af3061efa057c1c211b64343fb7ee10e820f
paulcervov <paulcervov@gmail.com>
2024-01-08 10:31:04 +0700

    Actualized arrays and strings

--- 2-values/values.go
+++ 2-values/values.go
@@ -11,4 +11,28 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+
+	// strings
+	var s1 = "Hello\table"
+	fmt.Println(s1) //Hello   able
+
+	var s2 = `Hello\table`
+	fmt.Println(s2) //Hello\table
+
+	var s3 = 'H'            //rune
+	fmt.Println(s3)         //72
+	fmt.Println(string(s3)) //H
+
+	fmt.Println(s1[0])         //72
+	fmt.Println(string(s1[0])) //H
+
+	// string in apostrophes can be multiline
+	var s4 = `
+One
+two
+three`
+	fmt.Println(s4)
+	//One
+	//two
+	//three
 }
